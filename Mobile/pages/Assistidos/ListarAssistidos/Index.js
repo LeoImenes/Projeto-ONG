130358efaf1fb13f6b6ef00655b2847b19efe2a9
jaquelinediasoliveira <jaquelinediasoliveira5@gmail.com>
2022-03-10 11:34:13 -0300

    Att 10/03

--- Mobile/pages/Assistidos/ListarAssistidos/Index.js
+++ Mobile/pages/Assistidos/ListarAssistidos/Index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
 
 import global from "../../Global/Style"
