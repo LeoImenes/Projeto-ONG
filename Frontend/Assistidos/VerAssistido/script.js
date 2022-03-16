b88e920d52b7a5948af685b7b7aabbab5c5c5ff1
LeoImenes <leonardoimenes@hotmail.com>
2022-03-16 17:59:38 -0300

    ver assistido finalizado

--- Frontend/Assistidos/VerAssistido/script.js
+++ Frontend/Assistidos/VerAssistido/script.js
@@ -1,6 +1,6 @@
 function menuDown() {
     let menuFam = document.querySelector(".dadosFamilia")
-    let littlearrow = document.querySelector(".arrow")
+    let littlearrow = document.querySelector(".faArrow")
     menuFam.classList.toggle(".faDown")
 
 
@@ -16,12 +16,46 @@ function menuDown() {
 }
 
 function menuDownAco() {
-    let menuAco = document.querySelector(".acompanhamento")
-    let littlearrow = document.querySelector(".arrow")
+    let menuAco = document.querySelector(".acolist")
+    let littlearrow = document.querySelector(".acoArrow")
     menuAco.classList.toggle(".acoDown")
 
 
     if (menuAco.classList.contains(".acoDown")) {
+        menuAco.style.display = "flex";
+        littlearrow.style.transform = "rotate(180deg)"
+
+
+    } else {
+        menuAco.style.display = "none"
+        littlearrow.style.transform = "rotate(0deg)"
+    }
+}
+
+function menuDownPsco() {
+    let menuAco = document.querySelector(".psclist")
+    let littlearrow = document.querySelector(".pscArrow")
+    menuAco.classList.toggle(".psDown")
+
+
+    if (menuAco.classList.contains(".psDown")) {
+        menuAco.style.display = "flex";
+        littlearrow.style.transform = "rotate(180deg)"
+
+
+    } else {
+        menuAco.style.display = "none"
+        littlearrow.style.transform = "rotate(0deg)"
+    }
+}
+
+function menuDownDoenca() {
+    let menuAco = document.querySelector(".doclist")
+    let littlearrow = document.querySelector(".doArrow")
+    menuAco.classList.toggle(".doDown")
+
+
+    if (menuAco.classList.contains(".doDown")) {
         menuAco.style.display = "flex";
         littlearrow.style.transform = "rotate(180deg)"
 
