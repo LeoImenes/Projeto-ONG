ae3f4c787ead9bb94c1e63c67b0f08ad94585253
LeoImenes <leonardoimenes@hotmail.com>
2022-03-18 18:00:15 -0300

    mudar layot cadastro

--- Frontend/Assistidos/ListarTodosAssistidos/script.js
+++ Frontend/Assistidos/ListarTodosAssistidos/script.js
@@ -1,6 +1,9 @@
 function list() {
+    let input = document.querySelector('input');
+    let filterText = input.value
+    let names = []
     var body = document.querySelector(body)
-    fetch("http://10.87.207.27:3000/assistidos")
+    fetch("http://10.87.207.27:3000/Assistidos")
         .then(response => { return response.json() })
         .then(data => {
             data.forEach(fun => {
@@ -8,29 +11,92 @@ function list() {
                 var divnome = document.createElement("div")
                 var cont = document.querySelector(".content")
                 var img = document.createElement("img");
-                var cardassistido = document.createElement("div");
-                var nome = document.createElement("h1");
+                var cardAssistido = document.createElement("div");
+                var nomeFun = document.createElement("h1");
+                var matricula = document.createElement("h3");
 
-                cardassistido.className = "cardAssistido"
+                names.push(fun.nome_completo)
+                console.log(names)
+
+                cardAssistido.className = "cardAssistido"
                 img.className = "fotoUsuario"
                 divimg.className = "img"
+                divnome.className = "nome"
 
                 if (fun.foto == null) {
                     img.src = "../../Assets/icones/user.png"
                 } else {
-                    img.src = fun.btoa(fun.foto)
+                    img.src = fun.foto
                 }
 
-                nome.innerHTML = `${fun.nome_completo}`
+                if (!fun.status == 0) {
+                    matricula.innerHTML = `Ativo`
+                    matricula.style.color = `green`
+                } else {
+                    matricula.innerHTML = `Inativo`
+                    matricula.style.color = `red`
+                }
+
+                nomeFun.innerHTML = `${fun.nome_completo}`
+
 
                 divimg.appendChild(img)
-                divnome.appendChild(nome)
+                divnome.appendChild(nomeFun)
                 divnome.appendChild(matricula)
-                cardfuncionario.appendChild(divimg)
-                cardfuncionario.appendChild(divnome)
-                cont.appendChild(cardassistido)
+                cardAssistido.appendChild(divimg)
+                cardAssistido.appendChild(divnome)
+                cont.appendChild(cardAssistido)
 
 
             })
+
+
         })
+}
+
+function buscar() {
+    let input = document.getElementById("inp").value.toLowerCase();
+    let filtro = document.querySelectorAll("h1");
+    let card = document.querySelectorAll(".cardAssistido")
+
+    filtro.forEach((item, index) => {
+        (item.innerHTML.toLowerCase().includes(input)) ? card[index].style.display = "flex": card[index].style.display = "none";
+    })
+}
+
+function buscarInativos() {
+    let filtro = document.querySelectorAll("h3");
+    let card = document.querySelectorAll(".cardAssistido")
+    let inativo = document.getElementById("ina")
+
+    if (inativo.checked == 1) {
+        inativo.value = "i"
+        filtro.forEach((item, index) => {
+            (item.innerHTML.toLowerCase().startsWith(inativo.value)) ? card[index].style.display = "flex": card[index].style.display = "none";
+        })
+    } else {
+        filtro.forEach((item, index) => {
+            card[index].style.display = "flex";
+        })
+    }
+}
+
+
+function buscarAtivos() {
+    let filtro = document.querySelectorAll("h3");
+    let card = document.querySelectorAll(".cardAssistido")
+    let ativo = document.getElementById("ati")
+
+    if (ativo.checked == 1) {
+
+        ativo.value = "a"
+        filtro.forEach((item, index) => {
+            (item.innerHTML.toLowerCase().startsWith(ativo.value)) ? card[index].style.display = "flex": card[index].style.display = "none";
+        })
+    } else {
+        filtro.forEach((item, index) => {
+            card[index].style.display = "flex";
+        })
+    }
+
 }
