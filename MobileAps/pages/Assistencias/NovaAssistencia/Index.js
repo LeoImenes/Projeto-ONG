68e185450e63f299e56db952d720729d49863ece
jaquelinediasoliveira <jaquelinediasoliveira5@gmail.com>
2022-05-13 11:34:52 -0300

    att 13/05

--- MobileAps/pages/Assistencias/NovaAssistencia/Index.js
+++ MobileAps/pages/Assistencias/NovaAssistencia/Index.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, View, Text, StatusBar, ScrollView, TextInput, TouchableOpacity } from "react-native"
+import { StyleSheet, View, Text, StatusBar, ScrollView, TextInput, TouchableOpacity,ToastAndroid, } from "react-native"
 
 import global from "../../Global/Style"
 
-import Checkbox from 'expo-checkbox';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Ionicons, AntDesign } from '@expo/vector-icons';
 import { useFocusEffect } from '@react-navigation/native';
 import { Picker } from '@react-native-picker/picker';
@@ -16,10 +16,19 @@ export default function NovaAssistencia({ navigation }) {
     const [dados, setDados] = useState([]);
     const [selecionados, setSelecionados] = useState([]);
     const [ref, setRef] = useState([])
-    const [ut, setUt] = useState([])
-    const [est, setEst] = useState([])
-    const [coiso, setCoiso] = useState([])
     const [valuePicker, setValuePicker] = useState();
+    const [idFunc, setIdFunc] = useState();
+
+    const getFunc = async () => {
+        let value = await AsyncStorage.getItem('userdata');
+
+        fetch(`http://10.87.207.20:3000/funcionarios/${value}`)
+            .then(resp => { return resp.json() })
+            .then(async data => {
+                const id = JSON.parse(data[0].id_funcionario)
+                setIdFunc(id)
+            })
+    }
 
     const listar = () => {
         let newDados = [...dados];
@@ -33,6 +42,7 @@ export default function NovaAssistencia({ navigation }) {
         React.useCallback(() => {
             const data = new Date();
             setDataCriacao(data)
+            getFunc()
 
             // fetch(`http://192.168.0.104:3000/assistidos`)
             fetch(`http://10.87.207.20:3000/assistidos`)
@@ -47,28 +57,45 @@ export default function NovaAssistencia({ navigation }) {
                 .then(resp => { return resp.json() })
                 .then(data => {
 
-                    let tempA = [], tempB = [], tempC = [], tempD = [];
+                    let tempA = [], tempD = [];
 
                     data.forEach(item => {
-                        if (item.tipo == 1) {
+                        if (item.tipo == 0) {
                             tempA.push(item);
-                        } else if(item.tipo == 2){
-                            tempB.push(item);
-                        } else if(item.tipo == 3){
-                            tempC.push(item);
                         } else {
                             tempD.push(item);
                         }
                     })
 
                     setRef(tempA)
-                    setUt(tempB)
-                    setEst(tempC)
-                    setCoiso(tempD)
                 })
                 .catch(err => { console.log(err) });
         }, [])
     );
+
+    const cadastrar = () =>{
+        let item = {
+            "id_funcionario": idFunc,
+            "assistidos": selecionados,
+            "itens": valuePicker
+        }
+
+        fetch(`http://10.87.207.20:3000/funcionario/assistencias`, {
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": JSON.stringify(item),
+        })
+            .then(resp => { return resp.json() })
+            .then(async data => {
+                if (data.err !== undefined) {
+                    ToastAndroid.show('Falha ao registrar assistência!', ToastAndroid.SHORT)
+                } else {
+                    ToastAndroid.show('Resgitro efetuado!', ToastAndroid.SHORT)
+                }
+            })
+    }
 
     const add = (idAss, idCard) => {
         if (selecionados.includes(idAss)) selecionados.splice(selecionados.indexOf(idAss), 1)
@@ -82,18 +109,34 @@ export default function NovaAssistencia({ navigation }) {
                 hidden={false}
                 backgroundColor="transparent"
                 translucent={true} />
-            <View style={{ width: "100%", height: 150, backgroundColor: "#166B8A", borderBottomRightRadius: 40, borderBottomLeftRadius: 40, alignItems: "center", justifyContent: "center" }}>
-                <View style={css.filter}>
+            <View style={{ width: "100%", height: 200, backgroundColor: "#166B8A", borderBottomRightRadius: 40, borderBottomLeftRadius: 40, justifyContent: "center" }}>
+                <View style={{ width: "100%", height: "20%", flexDirection: "row", alignItems: "center", justifyContent: "space-evenly" }}>
                     <Ionicons name="arrow-back-circle-outline" size={34} color="white" onPress={() => { navigation.navigate("Home") }} />
-                    <TouchableOpacity style={{ width: "80%", height: "100%", justifyContent: "center" }} onPress={() => { listar() }}>
-                        <Text style={{ fontSize: 20, color: 'white' }}>Ordem alfabética</Text>
+                    <View style={{ width: "80%" }}>
+                        <Text style={{ color: "white", fontSize: 20, fontWeight: "bold" }}>Refeição:</Text>
+                    </View>
+                </View>
+                <View style={css.filter}>
+                    <View style={css.cards}>
+                        <Picker selectedValue={valuePicker} onValueChange={(itemValue, itemIndex) => setValuePicker(itemValue)}>
+                            <Picker.Item label={"Selecione..."} value={""} style={{ color: "gray" }}/>
+                            {
+                                ref.map((item, index) => {
+                                    return (
+                                        <Picker.Item label={item.item} value={item.id_item} key={index} />
+                                    )
+                                })
+                            }
+                        </Picker>
+                    </View>
+                    <TouchableOpacity style={{ width: "10%", height: "100%", alignItems: "center", justifyContent: "center"}} onPress={() => { listar() }}>
+                        <Text style={{ color: "white", fontSize: 20, fontWeight: "bold" }}>A-Z</Text>
                     </TouchableOpacity>
                 </View>
-                <View style={{ width: "90%", alignItems: "center", alignSelf: "center", height: "30%", justifyContent: "space-around", flexDirection: "row" }}>
-                    <Text style={{ fontWeight: "bold", fontSize: 20, color: "white" }}>Nova Assistência</Text>
-                </View>
             </View>
-            <View style={{ width: "100%", height: 450, borderBottomWidth: 3, borderBottomColor: "#166B8A", marginBottom: 20}}>
+            <Text style={{ color: "black", fontSize: 20, fontWeight: "bold", marginTop: 5}}>Selecione:</Text>
+            <Text style={{ color: "#166B8A", fontSize: 20, fontWeight: "bold" }}>- - - - - - - - - - - - - - - - - - - - - - </Text>
+            <View style={{ width: "100%", height: 450 }}>
                 <ScrollView>
                     {
                         lista.map((item, index) => {
@@ -104,51 +147,10 @@ export default function NovaAssistencia({ navigation }) {
                     }
                 </ScrollView>
             </View>
-            <Text style={global.textInfo1}>Item solicitado:</Text>
-            <View style={{ width: "90%", alignSelf: "center", borderBottomWidth: 2, marginTop: 10}}>
-                <Picker selectedValue={valuePicker} onValueChange={(itemValue, itemIndex) => setValuePicker(itemValue)}>
-                    {
-                        ref.map((item, index) => {
-                            return (
-                                <Picker.Item label={item.item} value={item.id_item} key={index} />
-                            )
-                        })
-                    }
-                </Picker>
-            </View>
-            <View style={{ width: "90%", alignSelf: "center", borderBottomWidth: 2, marginTop: 10}}>
-                <Picker selectedValue={valuePicker} onValueChange={(itemValue, itemIndex) => setValuePicker(itemValue)}>
-                    {
-                        ut.map((item, index) => {
-                            return (
-                                <Picker.Item label={item.item} value={item.id_item} key={index} />
-                            )
-                        })
-                    }
-                </Picker>
-            </View>
-            <View style={{ width: "90%", alignSelf: "center", borderBottomWidth: 2, marginTop: 10}}>
-                <Picker selectedValue={valuePicker} onValueChange={(itemValue, itemIndex) => setValuePicker(itemValue)}>
-                    {
-                        est.map((item, index) => {
-                            return (
-                                <Picker.Item label={item.item} value={item.id_item} key={index} />
-                            )
-                        })
-                    }
-                </Picker>
-            </View>
-            <View style={{ width: "90%", alignSelf: "center", borderBottomWidth: 2, marginTop: 10}}>
-                <Picker selectedValue={valuePicker} onValueChange={(itemValue, itemIndex) => setValuePicker(itemValue)}>
-                    {
-                        coiso.map((item, index) => {
-                            return (
-                                <Picker.Item label={item.item} value={item.id_item} key={index} />
-                            )
-                        })
-                    }
-                </Picker>
-            </View>
+            <Text style={{ color: "#166B8A", fontSize: 20, fontWeight: "bold" }}>- - - - - - - - - - - - - - - - - - - - - - </Text>
+            <TouchableOpacity style={{ backgroundColor: "rgb(22,107,138)", width: "35%", height: 45, alignItems: "center", justifyContent: "center", borderRadius: 5, marginTop: "5%", alignSelf: "center", marginBottom: "20%" }} onPress={() => {cadastrar()}}>
+                <Text style={global.buttonText1}>SALVAR</Text>
+            </TouchableOpacity>
         </View>
     );
 }
@@ -165,13 +167,17 @@ const css = StyleSheet.create({
     },
     filter: {
         width: "100%",
-        height: "30%",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-evenly",
+        height: "40%",
+        flexDirection: "row", 
         alignItems: "center",
-        alignSelf: "flex-end",
-        marginTop: "8%",
-        marginBottom: "1%"
+        justifyContent: "space-evenly"
     },
+    cards: {
+        width: 200,
+        height: 60,
+        borderBottomWidth: 2,
+        marginTop: 10,
+        backgroundColor: "rgba(255, 255, 255, 0.8)",
+        borderRadius: 10,
+    }
 });
