a417514eedb7f4888f61d9b2c329ef348eb5f7c3
jaquelinediasoliveira <jaquelinediasoliveira5@gmail.com>
2022-06-10 11:38:04 -0300

    att

--- pdfTeste/teste/index.js
+++ pdfTeste/teste/index.js
@@ -5,17 +5,17 @@ btn.addEventListener("click", ()=>{
 })
 function CriaPDF() {
     
-    var minhaTabela = document.getElementById('tabela').innerHTML;
-    var style = "<style>";
-    style = style + "table {width: 100%;font: 20px Calibri;}";
-    style = style + "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
-    style = style + "padding: 2px 3px;text-align: center;}";
-    style = style + "</style>";
+    var minhaTabela = document.getElementById('pdf').innerHTML;
+    // var style = "<style>";
+    // style = style + "table {width: 100%;font: 20px Calibri;}";
+    // style = style + "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
+    // style = style + "padding: 2px 3px;text-align: center;}";
+    // style = style + "</style>";
     // CRIA UM OBJETO WINDOW
     var win = window.open('', '', 'height=700,width=700');
     win.document.write('<html><head>');
-    win.document.write('<title>Empregados</title>');   // <title> CABEÇALHO DO PDF.
-    win.document.write(style);                         // INCLUI UM ESTILO NA TAB HEAD
+    // win.document.write('<title>Empregados</title>');   // <title> CABEÇALHO DO PDF.
+    // win.document.write(style);                      // INCLUI UM ESTILO NA TAB HEAD
     win.document.write('</head>');
     win.document.write('<body>');
     win.document.write(minhaTabela);                   // O CONTEUDO DA TABELA DENTRO DA TAG BODY
