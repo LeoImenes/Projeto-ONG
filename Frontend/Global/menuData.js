32addb2216e2b173c0edb4f65ff7e8cb2efb71f2
LeoImenes <leonardoimenes@hotmail.com>
2022-06-10 07:45:41 -0300

    att

--- Frontend/Global/menuData.js
+++ Frontend/Global/menuData.js
@@ -6,12 +6,15 @@ function getfunc() {
     let foto = document.querySelector(".userimg");
     user.innerHTML = funcionario.nome_completo
         // foto.src = funcionario.foto
+        
+
+        
 
     fetch(`${url}/funcionarios/${funcionario.matricula}`)
         .then(resp => { return resp.json() })
         .then(data => {
             user.innerHTML = data[0].nome_completo
-            if ((data[0].foto == null)) {
+            if ((data[0].foto == null) || data[0].foto == 'undefined') {
                 foto.src = `../../Assets/icones/user.png`
             } else {
                 foto.src = data[0].foto
